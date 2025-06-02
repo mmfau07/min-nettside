1f5f1412c9bfa3d02cee2bd27bd6eb41107dbddc
Mokumokuren <mmfau07gmail.com>
2025-06-02 17:25:00 +0200

    sat op comblingen melom forsjelige pages. få alle neste er server fungsjonene

--- client/src/conecter.tsx
+++ client/src/conecter.tsx
@@ -1,9 +1,17 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './login/page';
 
 
 function Conecter() {
 	return (
 		<div className="Conecter">
-			
+			<Router>
+				<Routes>
+					<Route path='/login' element={<Login />}></Route>
+					<Route path="/" element={<Navigate to="/login" />} />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
